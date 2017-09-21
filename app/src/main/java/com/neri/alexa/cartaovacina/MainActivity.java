d08d142548953755e65e6124b3e9513937e6bb2a
Bruno Pinheiro <bruno1_@msn.com>
2017-09-20 22:05:45 -0300

    Revert "Limpeza no código"

--- app/src/main/java/com/neri/alexa/cartaovacina/MainActivity.java
+++ app/src/main/java/com/neri/alexa/cartaovacina/MainActivity.java
@@ -1,31 +1,39 @@
 package com.neri.alexa.cartaovacina;
 
-import android.app.Activity;
 import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends Activity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    @Override
-    public void onClick(View v) {
-        Intent intent;
-        switch (v.getId()) {
-            case R.id.buttonCadastro:
-                intent = new Intent(MainActivity.this, CadastroActivity.class);
-                startActivity(intent);
-                break;
 
-            case R.id.buttonImc:
-                intent = new Intent(MainActivity.this, ImcActivity.class);
+        Button btnTelaCadastro =  (Button) findViewById(R.id.buttonCadastro);
+        Button btnTelaImc = (Button) findViewById(R.id.buttonImc);
+
+        btnTelaCadastro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,CadastroActivity.class );
                 startActivity(intent);
-                break;
-        }
+            }
+        });
+
+        btnTelaImc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,ImcActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
     }
 }
